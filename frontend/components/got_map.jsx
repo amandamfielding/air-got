d34fc7ee53580a9cd196fa357fb16b35d6481a9e
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2016-11-08 09:14:08 -0800

    minor updates in css files

--- frontend/components/got_map.jsx
+++ frontend/components/got_map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router';
-/* globals google */
+
 
 function getNormalizedCoord(coord, zoom) {
   var y = coord.y;
