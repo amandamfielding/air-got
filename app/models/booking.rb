028e40731e3234786a7b56a44f74b7a5c921940b
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2016-11-25 10:21:30 -0800

    add date validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,7 +14,16 @@
 
 class Booking < ActiveRecord::Base
   validates :place, :user, :check_in_date, :check_out_date, :number_of_guests, presence: true
-
+  validates :check_in_date, :check_out_date, overlap: { scope: 'place_id',message_content: 'or check out date already has a booking during that time. Please choose others dates.' }
+  validate :check_out_date_is_after_check_in_date, on: :create
   belongs_to :place
   belongs_to :user
+
+  def check_out_date_is_after_check_in_date()
+    return if check_out_date.blank? || check_in_date.blank?
+    if check_out_date < check_in_date
+      errors.add(:check_out_date, "cannot be before the check in date")
+    end
+  end
+
 end
