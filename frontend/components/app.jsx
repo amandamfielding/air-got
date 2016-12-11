1718771cb6c83b321d05921e3335c55d6ddc412c
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2016-12-11 14:25:33 -0800

    add transition between routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import HeaderContainer from './header/header_container';
 import Footer from './footer/footer';
 import Home from './home/home';
+import EasyTransition from 'react-easy-transition'
 
+const App = ({ children, location }) => (
 
-const App = ({ children }) => (
-  <div>
-    <HeaderContainer />
-    {children}
-    <Footer />
-  </div>
+    <EasyTransition
+        path={location.pathname}
+        initialStyle={{opacity: 0}}
+        transition="opacity 0.1s ease-in"
+        finalStyle={{opacity: 1}}
+    >
+        <HeaderContainer />
+        {children}
+        <Footer />
+    </EasyTransition>
 );
 
 export default App;
